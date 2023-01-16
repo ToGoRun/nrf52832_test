eff71bc89f5e4ddabb0df8a87d4959594787bfea
xiaochengcheng <xiaochengcheng@nooploop.com>
2023-01-16 19:01:17 +0800

    加入ulog打印功能。

--- applications/main.c
+++ applications/main.c
@@ -11,6 +11,7 @@
 
 #include <rtthread.h>
 #include <rtdevice.h>
+#include <ulog.h>
 #define DK_BOARD_LED_1  17
 #define DK_BOARD_LED_2  18
 
@@ -18,7 +19,8 @@ int main(void)
 {
     int count = 1;
     rt_pin_mode(DK_BOARD_LED_1, PIN_MODE_OUTPUT);
-
+    LOG_I("LOG_Printf_info\n");
+    LOG_D("LOG_Printf_debug\n");
     while (count++)
     {
         rt_pin_write(DK_BOARD_LED_1, PIN_HIGH);
@@ -26,6 +28,7 @@ int main(void)
 
         rt_pin_write(DK_BOARD_LED_1, PIN_LOW);
         rt_thread_mdelay(500);
+        LOG_D("LED_used\n");
     }
     return RT_EOK;
 }
