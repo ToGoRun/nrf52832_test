dce2eea65efc32c092aa0ad4795b8828539c7f5a
xiaochengcheng <xiaochengcheng@nooploop.com>
2023-01-16 15:53:17 +0800

    添加nrf52832工程模板

---
+++ rtconfig_preinc.h
@@ -0,0 +1,16 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define HAVE_CCONFIG_H
+#define NRF52832_XXAA
+#define RT_USING_LIBC
+#define RT_USING_NEWLIB
+#define USE_APP_CONFIG
+#define _POSIX_C_SOURCE 1
+#define __RTTHREAD__
+
+#endif /*RTCONFIG_PREINC_H__*/
